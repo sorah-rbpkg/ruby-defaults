08591ef03d82136558aa8831f57194c6e3da6108
Antonio Terceiro <terceiro@debian.org>
2018-02-25 20:40:40 -0300

    Simplify format of minimum Ruby version

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -6,20 +6,20 @@ module RubyDebianDev
     RUBY_INTERPRETERS[ruby] = yield
   end
 
-  has_support_for 'ruby2.4' do
+  has_support_for 'ruby2.5' do
     {
       version:             '2.5',
       binary:              '/usr/bin/ruby2.5',
       api_version:         '2.5.0',
       shared_library:      'libruby2.5',
-      min_ruby_dependency: 'ruby (>= 1:2.5~0)',
+      min_ruby_version:    '1:2.5~0',
     }
   end
 
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
-        return data[:min_ruby_dependency]
+        return "ruby (>= %s)" % data[:min_ruby_version]
       end
     end
     return nil
