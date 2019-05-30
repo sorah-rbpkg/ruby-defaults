4d1732c72a43e9384719bb685ba23c9d18bd2fa8
Sorah Fukumori <sorah@cookpad.com>
2019-05-31 04:48:05 +0900

    Ruby 2.7

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -17,6 +17,19 @@ module RubyDebianDev
     }
   end
 
+  has_support_for 'ruby2.7' do
+    {
+      version:             '2.7',
+      binary:              '/usr/bin/ruby2.7',
+      api_version:         '2.7.0',
+      shared_library:      'libruby2.7',
+      min_ruby_version:    '1:2.7~0',
+      ruby_upper_bound:    '1:2.8~',
+    }
+  end
+
+
+
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
