dc743bc08d0d61ea44014395b07aa413c11c32e2
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2020-10-31 15:55:59 -0300

    Add support for ruby3.0 as an alternative interpreter

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -19,6 +19,17 @@ module RubyDebianDev
     }
   end
 
+  has_support_for 'ruby3.0' do
+    {
+      version:             '3.0',
+      binary:              '/usr/bin/ruby3.0',
+      api_version:         '3.0.0',
+      shared_library:      'libruby3.0',
+      min_ruby_version:    '1:3.0~0',
+      ruby_upper_bound:    '1:3.1~',
+    }
+  end
+
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
