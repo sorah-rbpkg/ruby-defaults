2bdb154092c0e556839fe3598af7f4b9d795e033
Antonio Terceiro <terceiro@debian.org>
2022-04-23 12:05:18 -0300

    Add support for ruby3.1

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -19,6 +19,17 @@ module RubyDebianDev
     }
   end
 
+  has_support_for 'ruby3.1' do
+    {
+      version:             '3.1',
+      binary:              '/usr/bin/ruby3.1',
+      api_version:         '3.1.0',
+      shared_library:      'libruby3.1',
+      min_ruby_version:    '1:3.1~0',
+      ruby_upper_bound:    '1:3.2~',
+    }
+  end
+
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
